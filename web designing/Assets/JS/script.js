4a4ee5289f87f2080349a723bedc0902f745f169
Abdul-Basit-cmd <abasit20062006@gmail.com>
2024-11-28 12:16:39 +0500

    done work

---
+++ web designing/Assets/JS/script.js
@@ -0,0 +1,26 @@
+
+
+let enteryourname = 88;
+let requiredage = 18;
+
+
+if (enteryourname > requiredage) {
+    console.log("you can drive")
+}
+else {
+    console.log("you cannot drive")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
